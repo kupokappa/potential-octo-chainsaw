3186bae41555546bd5a2ccbc26ab656659133629
kupokappa <bperkins839@gmail.com>
2022-09-08 02:45:04 -0700

    set up input event functions move serial function delegates from UserPawn class

--- Source/SwitchInputPassthru/Private/Core/InputEmuGameMode.cpp
+++ Source/SwitchInputPassthru/Private/Core/InputEmuGameMode.cpp
@@ -4,12 +4,101 @@
 #include "Core/SerialPort.h"
 #include "Macros.h"
 
-AInputEmuGameMode::AInputEmuGameMode() {
+// Create shared reference construct for FSerialPort
+TSharedRef<FSerialPort> serialPort(new FSerialPort);
 
+AInputEmuGameMode::AInputEmuGameMode() {}
+
+void AInputEmuGameMode::HandleABtnPressed() {
+	if (!serialPort.Get().GetIsConnected()) {
+#ifdef UE_BUILD_DEBUG
+		Log("Initializing port");
+#endif
+		// Connect to the USB-UART bridge at the specified port, with the specified baud rate
+		if (InitSerial.IsBound()) {
+			// The actual name of the port we'll connect to
+			const char* portName = TCHAR_TO_ANSI(*InPortName);
+			InitSerial.Execute(portName, (uint32_t)baudRate);
+		}
+	}
+}
+// Face buttons
+void AInputEmuGameMode::HandleABtnReleased() {}
+void AInputEmuGameMode::HandleBBtnPressed() {
+	if (serialPort.Get().GetIsConnected()) {
+		if (CloseSerial.IsBound()) {
+			CloseSerial.Execute();
+		}
+	}
+}
+void AInputEmuGameMode::HandleBBtnReleased() {}
+void AInputEmuGameMode::HandleXBtnPressed() {
+	if (SyncBridge.IsBound()) {
+		if (serialPort.Get().GetIsConnected()) {
+			SyncBridge.Execute();
+		}
+	}
+}
+void AInputEmuGameMode::HandleXBtnReleased() {}
+void AInputEmuGameMode::HandleYBtnPressed() {}
+void AInputEmuGameMode::HandleYBtnReleased() {}
+
+void AInputEmuGameMode::HandlePlusBtnPressed() {}
+void AInputEmuGameMode::HandlePlusBtnReleased() {}
+void AInputEmuGameMode::HandleMinusBtnPressed() {}
+void AInputEmuGameMode::HandleMinusBtnReleased() {}
+void AInputEmuGameMode::HandleHomeBtnPressed() {}
+void AInputEmuGameMode::HandleHomeBtnReleased() {}
+void AInputEmuGameMode::HandleCaptureBtnPressed() {}
+void AInputEmuGameMode::HandleCaptureBtnReleased() {}
+
+// Stick buttons
+void AInputEmuGameMode::HandleLStickBtnPressed() {}
+void AInputEmuGameMode::HandleLStickBtnReleased() {}
+void AInputEmuGameMode::HandleRStickBtnPressed() {}
+void AInputEmuGameMode::HandleRStickBtnReleased() {}
+
+// Shoulder buttons
+void AInputEmuGameMode::HandleLBtnPressed() {}
+void AInputEmuGameMode::HandleLBtnReleased() {}
+void AInputEmuGameMode::HandleRBtnPressed() {}
+void AInputEmuGameMode::HandleRBtnReleased() {}
+void AInputEmuGameMode::HandleZLBtnPressed() {}
+void AInputEmuGameMode::HandleZLBtnReleased() {}
+void AInputEmuGameMode::HandleZRBtnPressed() {}
+void AInputEmuGameMode::HandleZRBtnReleased() {}
+
+// Hat
+void AInputEmuGameMode::HandleUpBtnPressed() {}
+void AInputEmuGameMode::HandleUpBtnReleased() {}
+void AInputEmuGameMode::HandleDownBtnPressed() {}
+void AInputEmuGameMode::HandleDownBtnReleased() {}
+void AInputEmuGameMode::HandleLeftBtnPressed() {}
+void AInputEmuGameMode::HandleLeftBtnReleased() {}
+void AInputEmuGameMode::HandleRightBtnPressed() {}
+void AInputEmuGameMode::HandleRightBtnReleased() {}
+
+// Called when the game starts or when spawned
+void AInputEmuGameMode::BeginPlay() {
+	// Bind serial event delegates
+	InitSerial.BindSP(serialPort, &FSerialPort::Initialize);
+	CloseSerial.BindSP(serialPort, &FSerialPort::Disconnect);
+	SyncBridge.BindSP(serialPort, &FSerialPort::Sync);
+
+	Super::BeginPlay();
 }
 
-void AInputEmuGameMode::BeginPlay() {
-	Super::BeginPlay();
+// Called every frame
+void AInputEmuGameMode::Tick(float DeltaTime) {
+
+	Super::Tick(DeltaTime);
+}
+
+void AInputEmuGameMode::EndPlay(const EEndPlayReason::Type endPlayReason) {
+	// Close the serial port before quitting, if it's open
+	CloseSerial.ExecuteIfBound();
+
+	Super::EndPlay(endPlayReason);
 }
 
 AInputEmuGameMode::~AInputEmuGameMode() {
--- Source/SwitchInputPassthru/Public/Core/InputEmuGameMode.h
+++ Source/SwitchInputPassthru/Public/Core/InputEmuGameMode.h
@@ -6,9 +6,6 @@
 #include "GameFramework/GameModeBase.h"
 #include "InputEmuGameMode.generated.h"
 
-/**
- *
- */
 UCLASS()
 class SWITCHINPUTPASSTHRU_API AInputEmuGameMode : public AGameModeBase {
 	GENERATED_BODY()
@@ -16,12 +13,79 @@ class SWITCHINPUTPASSTHRU_API AInputEmuGameMode : public AGameModeBase {
 public:
 	AInputEmuGameMode();
 
+	// User-input port name
 	UPROPERTY(EditAnywhere)
-	FString PortName;
+	FString InPortName = "\\\\.\\COM3";
+
+	// Probably won't change the baud rate from this
+	UPROPERTY(EditAnywhere)
+	uint32 baudRate = 19200;
+
+	// Declare and assign SerialPort::Initialize() delegate
+	DECLARE_DELEGATE_TwoParams(InitSerialDelegate, const char*, unsigned long)
+	InitSerialDelegate InitSerial;
+
+	// Declare and assign SerialPort::Disconnect() delegate
+	DECLARE_DELEGATE(CloseSerialDelegate)
+	CloseSerialDelegate CloseSerial;
+
+	DECLARE_DELEGATE(SyncBridgeDelegate)
+	SyncBridgeDelegate SyncBridge;
+
+	// Face buttons
+	void HandleABtnPressed();
+	void HandleABtnReleased();
+	void HandleBBtnPressed();
+	void HandleBBtnReleased();
+	void HandleXBtnPressed();
+	void HandleXBtnReleased();
+	void HandleYBtnPressed();
+	void HandleYBtnReleased();
+
+	void HandlePlusBtnPressed();
+	void HandlePlusBtnReleased();
+	void HandleMinusBtnPressed();
+	void HandleMinusBtnReleased();
+	void HandleHomeBtnPressed();
+	void HandleHomeBtnReleased();
+	void HandleCaptureBtnPressed();
+	void HandleCaptureBtnReleased();
+
+	// Stick buttons
+	void HandleLStickBtnPressed();
+	void HandleLStickBtnReleased();
+	void HandleRStickBtnPressed();
+	void HandleRStickBtnReleased();
+
+	// Shoulder buttons
+	void HandleLBtnPressed();
+	void HandleLBtnReleased();
+	void HandleRBtnPressed();
+	void HandleRBtnReleased();
+	void HandleZLBtnPressed();
+	void HandleZLBtnReleased();
+	void HandleZRBtnPressed();
+	void HandleZRBtnReleased();
+
+	// Hat
+	void HandleUpBtnPressed();
+	void HandleUpBtnReleased();
+	void HandleDownBtnPressed();
+	void HandleDownBtnReleased();
+	void HandleLeftBtnPressed();
+	void HandleLeftBtnReleased();
+	void HandleRightBtnPressed();
+	void HandleRightBtnReleased();
+
+	void ConnectToBridge();
 
 	~AInputEmuGameMode();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// Called when the game ends
+	virtual void EndPlay(const EEndPlayReason::Type endPlayReason) override;
 };
